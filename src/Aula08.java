350ac448cc5de618ca96369bd728d7516944a930
GCMD3V <gabrielmac59@gmail.com>
2023-05-15 22:12:40 -0300

    att

--- src/Aula08.java
+++ src/Aula08.java
@@ -16,30 +16,31 @@ public class Aula08 {
     }
 }
 
-
-
+// NOVO PROJETO
 //git init
 //git add .
 //git commit -m "projeto criado"
 //git branch "nome_do_branch"  -> um branch é uma ramificação independente de desenvolvimento que permite que você trabalhe em diferentes versões do seu projeto simultaneamente.
 //git push -u nome_do_branch
 
-
+//UPDATE/ALTERAÇÃO/ATUALIZAÇÃO
 // enviar alteração
 // faz uma cópia/clone da versão anterior
 
 // git add .
-// git commit -m "nome_do_branch" -> o uso do comando -m é apenas para criar uma mensagem. Sem o -m é marcado um novo registro da alteração dentro dagit ramificação.
+// git commit -m "nome da atualização" -> o uso do comando -m é apenas para criar uma mensagem. Sem o -m é marcado um novo registro da alteração dentro dagit ramificação.
 // git push origin nome_do_branch  
 
+//COMANDOS ÚTEIS
+// git add src/Aula08.java  -> seleciona arquivos para salvar no repositório LOCAL a BRANCH atual
+// git commit -m "Salvar alterações antes de alternar para a branch 'alteracao'" -> salva o os arquivos adicinados ao respositório LOCAL
 
-// Caso use o código abaixo não é necessário usar o branch quando atualiza
+// git checkout nome_do_branch -> seleciona a branch, ou seja, permite navegar entre as branch's (ramificações)
+
+
+
+// Caso use o código abaixo não é necessário citar o nome_do_branch quando atualiza uma nova versão
 // git push (caso tenha executado o comando -> git push --set-upstream origin newVersion)
-
-
-//movimentação entre os branch's
-// git checkout nome_do_branch
-// git checkout feature/alteracao
 
 
 // como mostrar todos os branch's
@@ -50,3 +51,5 @@ public class Aula08 {
 // como remover os branch's
 // git branch -d nome_do_branch
 // git branch -d Aula.08new
+
+
