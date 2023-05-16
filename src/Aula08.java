472a4f0912a571cf807a0bd9da905415a1ba9e18
GCMD3V <gabrielmac59@gmail.com>
2023-05-15 21:04:51 -0300

    nova versão

--- src/Aula08.java
+++ src/Aula08.java
@@ -18,7 +18,10 @@ public class Aula08 {
 //git commit -m "projeto criado"
 //git branch Aula.08
 //git push -u origin Aula.08
+
+
+// enviar alteração
+// faz uma cópia/clone da versão anterior
+//
+
 //git push origin Aula.08 (update)
-
-
-
