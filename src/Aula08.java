32bdab856e1b4f66dc08732744a66f69e6cf5ad8
GCMD3V <gabrielmac59@gmail.com>
2023-05-15 21:47:09 -0300

    add sucesso

--- src/Aula08.java
+++ src/Aula08.java
@@ -2,6 +2,8 @@ public class Aula08 {
     public static void main(String[] args) throws Exception {
         System.out.println("Vamos em frente!");
         System.out.println("Adicionar GitHub");
+        System.out.println("Adicionar newVersion");
+
 
 
 
@@ -25,6 +27,24 @@ public class Aula08 {
 // faz uma cópia/clone da versão anterior
 
 // git add .
-// git commit -m "nome_do_branch"
+// git branch "nome_do_branch"  -> um branch é uma ramificação independente de desenvolvimento que permite que você trabalhe em diferentes versões do seu projeto simultaneamente.
+// git commit -m "nome_do_branch" -> o uso do comando -m é apenas para criar uma mensagem. Sem o -m é marcado um novo registro da alteração dentro da ramificação.
+// git push origin nome_do_branch  
+
+
+// Caso use o código abaixo não é necessário usar o branch quando atualiza
 // git push (caso tenha executado o comando -> git push --set-upstream origin newVersion)
-// ou use git push origin nome_do_branch  
+
+
+//movimentação entre os branch's
+// git checkout nome_do_branch
+
+
+// como mostrar todos os branch's
+// git branch 
+// git status -> O comando git statusé usado para exibir o estado atual do seu repositório Git. Ele fornece informações sobre os arquivos modificados, adicionados ou excluídos no seu diretório de trabalho, bem como informações sobre a filial atual e o estado do repositório em relação à filial remota.
+
+
+// como remover os branch's
+// git branch -d nome_do_branch
+// git branch -d Aula.08new
