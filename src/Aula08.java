94549418acbf80548c434bf94a3cca440a3edc86
GCMD3V <gabrielmac59@gmail.com>
2023-05-16 20:10:02 -0300

    novo

--- src/Aula08.java
+++ src/Aula08.java
@@ -8,6 +8,9 @@ public class Aula08 {
     }
 }
 
+//importante saber... o git não sobe pastas vazias
+//O terminal da pasta a ser salva deve estar selecionado no cd -> cd "F:\HTML SITES\0 CURSO SENAI\Aula08\Aula08"
+
 //INTERLIGANDO O GIT INSTALADO NA MÁQUINA À CONTA DO GIT HUB
 // git config --global user.name "USUÁRIO_DO_GIT"
 // git config --global user.email "EMAIL_CADASTRADO" 
@@ -17,21 +20,20 @@ public class Aula08 {
 //git add . -> adiciona todos os arqivos para serem armazenados no registro (é possível selecionar, exemplo: git add arquivo1.txt arquivo2.js ou git add caminho/do/diretorio/ ou até mesmo: arquivo1.txt arquivo2.js)
 //git commit -m "projeto criado" -> é um registro de uma alteração realizada, no caso, a criação do projeto, SALVA A ALTERAÇÃO NO RESPOSITÓRIO LOCAL
 //git branch -m "nome_do_branch"  -> Renomeia o branch master (padrão). Um branch é uma ramificação(filial) independente de desenvolvimento que permite que você trabalhe em diferentes versões do seu projeto simultaneamente.
-//git push -u nome_do_branch -> O comando "git push -u nome_do_branch" é usado para enviar (push) o branch local para um repositório remoto e configurar uma relação de acompanhamento (tracking) entre o branch local e o branch remoto. A opção "-u" ou "--set-upstream" é utilizada para configurar a relação de acompanhamento entre as filiais local e remota. Ao utilizar essa opção, você estará estabelecendo uma associação entre o ramal local e o ramal remoto correspondente, permitindo que você utilize o comando "git push" posteriormente sem a necessidade de especificar o ramal remoto e local hoje.
+//git push -u nome_do_branch -> O comando "git push -u nome_do_branch" é usado para enviar (push) o branch local para um repositório remoto e configurar uma relação de acompanhamento (tracking) entre o branch local e o branch remoto. A opção "-u" ou "--set-upstream" é utilizada para configurar a relação de acompanhamento entre as filiais local e remota. Ao utilizar essa opção, você estará estabelecendo uma associação entre o ramal local e o ramal remoto correspondente, permitindo que você utilize o comando "git push" posteriormente sem a necessidade de especificar o ramal remoto e local hoje. É o mesmo que git push --set-upstream origin nome_do_novo_branch.
 
 //UPDATE/ALTERAÇÃO/ATUALIZAÇÃO
 // Antes de enviar alteração podemos também fazer uma cópia/clone da versão anterior (git clone link_do_git)
 
 // git add .
-// git commit -m "nome da atualização" -> o uso do comando -m é apenas para criar uma mensagem/nome do registro. Sem o -m é aberta uma janela para inserir o texto.
+// git commit -m "nome da atualização" -> o uso do comando -m é apenas para criar um nome do registro. Sem o -m é aberta uma janela para inserir o texto.
 // git push origin nome_do_branch -> envia o commit ao branch situado no REPOSITÓRIO REMOTO 
 
 
 
 
 //COMANDOS ÚTEIS
-// git branch "nome_do_branch" -> cria uma nova ramificação sem excluir as anteriores
-// Se você quiser substituir o ramal padrão no repositório remoto, você também precisará atualizar o repositório remoto usando o comando git push --set-upstream origin nome_do_novo_branch para estabelecer o novo ramal como o padrão no repositório remoto.
+// git branch "nome_do_branch" -> cria uma nova ramificação sem excluir ou renomear as anteriores
 // git add src/Aula08.java  -> seleciona arquivos para salvar no repositório LOCAL a BRANCH atual
 // git commit -m "Salvar alterações antes de alternar para a branch 'alteracao'" -> salva o os arquivos adicinados ao respositório LOCAL
 
@@ -54,6 +56,13 @@ public class Aula08 {
 // como remover os branch's
 // git branch -d nome_do_branch
 // git branch -d newVersion
+
+// ++git branch -m nome_do_branch_antigo nome_do_branch_novo: Renomeia o branch antigo para o novo nome especificado. Se o branch novo já existir, o Git realizará a renomeação, substituindo o
+//  branch existente pelo novo nome. Caso contrário, o ramal atual será renomeado para o novo nome. 
+
+// ++git branch -M nome_do_branch_antigo nome_do_branch_novo: Renomeia o branch antigo para o novo
+//   nome especificado, independentemente do branch novo já existir ou não. Se o ramal novo já existir, ele será substituído pelo ramal antigo renomeado para o novo nome. Se o ramal novo não 
+//   existir, o ramal atual será renomeado para o novo nome.
 
 
 //ENTENDENDO UM POUCO MAIS SOBRE BRANCH
